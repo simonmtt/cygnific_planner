1d339cdeaedd4eb1e1d5160e169d0f1dfa2f82b5
Simon Malaret <simonmalaret@macbook-pro-de-simon.home>
2023-11-05 13:39:02 +0100

    Script that identifies the last session date when the rollup field updates.

---
+++ automations/refresh-trainers-last-session.js
@@ -0,0 +1,25 @@
+let inputConfig = input.config();
+let id = inputConfig.id;
+let trainersTable = await base.getTable('Trainers');
+let trainerRecord = await trainersTable.selectRecordAsync(id, {
+  fields: ['Most recent session end date', 'Session end dates']
+})
+
+let dates = trainerRecord.getCellValue('Session end dates');
+
+function findMostRecentDate(dates) {
+  if (dates.length === 0) {
+    return undefined;
+  }
+
+  let datesArray = [];
+  for (let date of dates) {
+    datesArray.push(new Date(date));
+  }
+  return new Date(Math.max(...datesArray));
+}
+
+const mostRecentDate = findMostRecentDate(dates);
+await trainersTable.updateRecordAsync(id, {
+  'Most recent session end date': mostRecentDate
+})
